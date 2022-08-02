8df133033317b4e070a8ecfd9ccad88f3a4b3fb9
contihan <daniel.hanak@continental-corporation.com>
2022-08-02 13:19:06 +0200

    add get villa by id

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -14,5 +14,11 @@ namespace MagicVilla_VillaAPI.Controllers
         {
             return VillaStore.villaList;
         }
+
+        [HttpGet("id")]
+        public VillaDTO GetVilla(int id)
+        {
+            return VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+        }
     }
 }
--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -7,7 +7,7 @@ namespace MagicVilla_VillaAPI.Data
         public static List<VillaDTO> villaList = new List<VillaDTO>()
         {
             new VillaDTO{ Id = 1, Name = "Pool View"},
-            new VillaDTO{ Id = 1, Name = "Beach View"}
+            new VillaDTO{ Id = 2, Name = "Beach View"}
         };
     }
 }
