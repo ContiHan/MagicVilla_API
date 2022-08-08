59a6eee012cc0f6fddad1846afb11b2174fc77b6
contihan <daniel.hanak@continental-corporation.com>
2022-08-08 11:49:19 +0200

    change interval and level of logging

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -3,8 +3,8 @@ using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
-    .WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+Log.Logger = new LoggerConfiguration().MinimumLevel.Information()
+    .WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Minute).CreateLogger();
 
 builder.Host.UseSerilog();
 
