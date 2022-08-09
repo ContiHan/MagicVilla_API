3812ea2a4a9aceb318298b12096bd8f22086e1c2
contihan <daniel.hanak@continental-corporation.com>
2022-08-09 14:57:22 +0200

    add as no tracking

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -3,6 +3,7 @@ using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
@@ -118,17 +119,21 @@ namespace MagicVilla_VillaAPI.Controllers
                 return BadRequest();
             }
 
-            var villa = new Villa
+            var villa = _db.Villas.FirstOrDefault(v => v.Id == id);
+            if (villa is null)
             {
-                Area = villaDTO.Area,
-                Amenity = villaDTO.Amenity,
-                Details = villaDTO.Details,
-                Name = villaDTO.Name,
-                ImageUrl = villaDTO.ImageUrl,
-                Rate = villaDTO.Rate,
-                Occupancy = villaDTO.Occupancy,
-                UpdateDate = DateTime.Now
-            };
+                return NotFound();
+            }
+            villa.Id = id;
+            villa.Area = villaDTO.Area;
+            villa.Amenity = villaDTO.Amenity;
+            villa.Details = villaDTO.Details;
+            villa.Name = villaDTO.Name;
+            villa.ImageUrl = villaDTO.ImageUrl;
+            villa.Rate = villaDTO.Rate;
+            villa.Occupancy = villaDTO.Occupancy;
+            villa.UpdateDate = DateTime.Now;
+
             _db.Villas.Update(villa);
             _db.SaveChanges();
 
@@ -146,7 +151,7 @@ namespace MagicVilla_VillaAPI.Controllers
                 return BadRequest();
             }
 
-            var villa = _db.Villas.FirstOrDefault(v => v.Id == id);
+            var villa = _db.Villas.AsNoTracking().FirstOrDefault(v => v.Id == id);
             if (villa is null)
             {
                 return NotFound();
@@ -154,13 +159,14 @@ namespace MagicVilla_VillaAPI.Controllers
 
             var villaDTO = new VillaDTO
             {
+                Id = villa.Id,
                 Area = villa.Area,
                 Amenity = villa.Amenity,
                 Details = villa.Details,
                 Name = villa.Name,
                 ImageUrl = villa.ImageUrl,
                 Rate = villa.Rate,
-                Occupancy = villa.Occupancy,
+                Occupancy = villa.Occupancy
             };
             patchVillaDTO.ApplyTo(villaDTO, ModelState);
             if (!ModelState.IsValid)
@@ -170,6 +176,7 @@ namespace MagicVilla_VillaAPI.Controllers
 
             var partialUpdatedVilla = new Villa
             {
+                Id = villaDTO.Id,
                 Area = villaDTO.Area,
                 Amenity = villaDTO.Amenity,
                 Details = villaDTO.Details,
@@ -177,7 +184,8 @@ namespace MagicVilla_VillaAPI.Controllers
                 ImageUrl = villaDTO.ImageUrl,
                 Rate = villaDTO.Rate,
                 Occupancy = villaDTO.Occupancy,
-                UpdateDate = DateTime.Now
+                UpdateDate = DateTime.Now,
+                CreatedDate = villa.CreatedDate
             };
 
             _db.Villas.Update(partialUpdatedVilla);
